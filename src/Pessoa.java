dbb88fae9c96b3cf6c6a6be189848577728aed37
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-01-14 08:09:36 -0300

    Classe Pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,3 +1,58 @@
-public class Pessoa {
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+public class Pessoa {
+    private Integer id;
+    private String nome;
+    private LocalDate dataNascimento;
+    private String cpf;
+    private ArrayList<Endereco> enderecos = new ArrayList<>();
+
+    public Pessoa(Integer id, String nome, LocalDate dataNascimento, String cpf, ArrayList<Endereco> enderecos) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.enderecos = enderecos;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public ArrayList<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(ArrayList<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
 }
