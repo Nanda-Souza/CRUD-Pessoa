61649aee2d7deb22bf9e292bd0be4dbe63f0228a
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-01-18 22:36:28 -0300

    Movendo as classes pessoa e endereço para o package entity

---
+++ src/Entity/Endereco.java
@@ -0,0 +1,99 @@
+package Entity;
+
+public class Endereco {
+    private Integer id;
+    private String rua;
+    private Integer numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+    public Endereco(Integer id, String rua, Integer numero, String bairro, String cidade, String estado, String cep) {
+        this.id = id;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+    }
+
+    @Override
+    public String toString() {
+        return  "    Id: " + id + "\n" +
+                "    Rua: " + rua + "\n" +
+                "    Numero: " + numero + "\n" +
+                "    Bairro: " + bairro + "\n" +
+                "    Cidade: " + cidade + "\n" +
+                "    Estado: " + estado + "\n" +
+                "    CEP: " + cep + "\n";
+    }
+
+    public static boolean enderecoValido(String endereco){
+        return endereco != null &&
+                endereco.matches("^(?=.*[A-Za-zÀ-ÿ])[A-Za-zÀ-ÿ0-9 .,\\\\-]+$") &&
+                endereco.length() > 1;
+    }
+
+    public static boolean cepValido(String cep) {
+        return cep != null &&
+                cep.matches("^\\d{8}$");
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
