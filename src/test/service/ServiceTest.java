ab69b36d6dc96c523c8a98af90c6ffbe7ad4f393
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-01-20 13:13:21 -0300

    Teste Unitario para metodo alterarPessoa da camada de service

--- src/test/service/ServiceTest.java
+++ src/test/service/ServiceTest.java
@@ -41,8 +41,7 @@ public class ServiceTest {
     @DisplayName("Deve criar endereço quando dados válidos forem fornecidos")
 
     void deveCriarEnderecoQuandoDadosValidosForemFornecidos() {
-        Endereco e = new Endereco(1, "7 de Abril", 15, "Centro", "Poa", "RS", "91750410");
-
+        Endereco e = service.criarEndereco(1, "7 de Abril", 15, "Centro", "Poa", "RS", "91750410");
         assertNotNull(e, "Objeto endereço não pode ser nulo");
 
     }
@@ -102,6 +101,48 @@ public class ServiceTest {
         Pessoa p = service.buscarPessoa(1);
 
         assertNotNull(p);
+
+    }
+
+    @Test
+    @DisplayName("Deve alterar os dados do usuario no repositorio quando dados válidos forem fornecidos")
+
+    void deveAlterarDadosdaPessoaNoRepositorioQuandoDadosValidosForemFornecidos() {
+        Endereco e = service.criarEndereco(1, "7 de Abril", 15, "Centro", "Poa", "RS", "91750410");
+        ArrayList<Endereco> enderecos = new ArrayList<>();
+        enderecos.add(e);
+        service.cadastrarUsuario("José Silva", LocalDate.parse("1988-05-05"), "00985497077", enderecos);
+
+        Endereco novoE = service.criarEndereco(1, "7 de Setembro", 15, "Centro", "Poa", "RS", "91750410");
+        ArrayList<Endereco> novoEndereco = new ArrayList<>();
+        novoEndereco.add(novoE);
+
+        boolean retorno = service.atualizarPessoa(1, "João Souza", null,"00985497079", novoEndereco );
+
+        assertTrue(retorno, "Quando o endereço é alterado deve retornar true.");
+        assertEquals("João Souza", repository.getPessoas().get(0).getNome(), "Repositório deve conter a pessoa João Souza após a alteração");
+        assertEquals("7 de Setembro", repository.getPessoas().get(0).getEnderecos().get(0).getRua(), "Repositório deve conter o endereço 7 de Setembro para pessoa João Souza após a alteração");
+
+    }
+
+    @Test
+    @DisplayName("Não Deve alterar os dados do usuario no repositorio quando não encontrar o id da pessoa")
+
+    void naoDeveAlterarDadosdaPessoaNoRepositorioQuandoNaoEncontrarOidDaPessoa() {
+        Endereco e = service.criarEndereco(1, "7 de Abril", 15, "Centro", "Poa", "RS", "91750410");
+        ArrayList<Endereco> enderecos = new ArrayList<>();
+        enderecos.add(e);
+        service.cadastrarUsuario("José Silva", LocalDate.parse("1988-05-05"), "00985497077", enderecos);
+
+        Endereco novoE = service.criarEndereco(1, "7 de Setembro", 15, "Centro", "Poa", "RS", "91750410");
+        ArrayList<Endereco> novoEndereco = new ArrayList<>();
+        novoEndereco.add(novoE);
+
+        boolean retorno = service.atualizarPessoa(2, "João Souza", null,"00985497079", novoEndereco );
+
+        assertFalse(retorno, "Quando o id da pessoa não é localizado endereço deve retornar false.");
+        assertEquals("José Silva", repository.getPessoas().get(0).getNome(), "Repositório deve conter a pessoa José Silva pois não teve alteração");
+        assertEquals("7 de Abril", repository.getPessoas().get(0).getEnderecos().get(0).getRua(), "Repositório deve conter o endereço 7 de Abril para pessoa José Silva pois não teve alteração");
 
     }
 
